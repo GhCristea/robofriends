5c7c8ce9753bd17de0aa848f746e314516ec5cd9
ghcristea <g.cristea@studenti.uniba.it>
2019-01-22 20:50:45 +0000

    added tachyon package

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Hello extends Component{
+	render(){
+		return (
+			<div className = "f1 tc">
+				<h1>Hello World</h1>
+			</div>);
+
+	}
+}
+
+export default Hello;
