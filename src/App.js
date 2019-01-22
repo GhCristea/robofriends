56cba33ad7e24ee4b324b66743ea34f3b9cbe4a2
ghcristea <g.cristea@studenti.uniba.it>
2019-01-22 22:54:57 +0000

    Added search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardList from './CardList'
+import SearchBox from './SearchBox'
 import {robots} from './robots';
 
 const App = () => {
 	return (
+		<div className = 'tc'>
+		<h1>RoboFriends</h1>
+		<SearchBox />
 		<CardList robots={robots}/>
+		</div>
 		);
 }
 
