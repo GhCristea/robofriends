c56b26df0302e2c7a3a9b90cc11423be5bd60c20
ghcristea <g.cristea@studenti.uniba.it>
2019-01-22 23:07:25 +0000

    Added state to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 
-const App = () => {
-	return (
-		<div className = 'tc'>
-		<h1>RoboFriends</h1>
-		<SearchBox />
-		<CardList robots={robots}/>
-		</div>
-		);
+
+class App extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+		robots : robots,
+		searchBox: SearchBox
+		}
+	}
+
+	render(){
+		return (
+			<div className = 'tc'>
+			<h1>RoboFriends</h1>
+			<SearchBox />
+			<CardList robots={this.state.robots}/>
+			</div>
+			);	
+	}
+	
 }
 
 export default App;
