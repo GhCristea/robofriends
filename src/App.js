453ee76df2feecc2bbd4b01e57dc7e7804fc4458
ghcristea <g.cristea@studenti.uniba.it>
2019-01-23 00:03:05 +0000

    Finished communication between searcBox and CardList

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ import {robots} from './robots';
 class App extends Component {
 
 	constructor(){
-		super();
+		super()
+
 		this.state = {
 		robots : robots,
-		searchBox: SearchBox
+		searchfield: ''
 		}
 	}
 
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+	}
+
 	render(){
+			const filterRobots = this.state.robots.filter( robot => {
+			return robot.name.toLowerCase().includes( this.state.searchfield.toLowerCase() )
+		})
+		
 		return (
 			<div className = 'tc'>
 			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={this.state.robots}/>
+			<SearchBox searchChange = {this.onSearchChange} />
+			<CardList robots={filterRobots}/>
 			</div>
 			);	
 	}
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import {robots} from './robots';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
 	return (
 			<div className = 'pa2'>
 				<input 
 				className = 'pa3 ba b--green bg-lightest-blue'
 				type='search' 
-				placeholder = 'search robots'/>
+				placeholder = 'search robots'
+				value = {searchfield}
+				onChange = {searchChange}
+				/>
 			</div>
 		);
 }
